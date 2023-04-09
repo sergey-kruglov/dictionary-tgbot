0264f564a10546ef3547eaa2cd1b2612c3b1b830
Sergey Kruglov <srgy.krglv@gmail.com>
2023-04-09 16:04:39 +0300

    Change help text

--- src/lib/help.ts
+++ src/lib/help.ts
@@ -1,16 +1,5 @@
 export const helpText = `
-This bot uses external API. 
-You need to obtain an API key here: https://rapidapi.com/dpventures/api/wordsapi
-
-After obtaining the API key, use the command /auth <key> to save your API key in the bot.
-Then you can write a word to get a definition and pronunciation.
-
-
 Command list:
-
-/auth - Authenticate with your token. Example: /auth <rapidapi-key>.
-
-/random - Get a random word.
 
 /help - Show this help message.
 
