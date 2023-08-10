31ef6170daf42419e1b55a43481cbe7785ce3d4c
Sergey Kruglov <srgy.krglv@gmail.com>
2023-08-10 22:26:45 +0300

    Update help text

--- src/lib/help.ts
+++ src/lib/help.ts
@@ -1,7 +1,14 @@
 export const helpText = `
 Command list:
 
-/help - Show this help message.
+/randomword - Get random word (random word)
+/removeword - Remove a word from saved words (removeWord hello)
+/setreminder - Turn on/off reminders (setReminder on)
+/settimeframe - Set time when you want to receive reminders (setTimeFrame 10:00 20:00)
+/setinterval - Set interval in minutes for reminders (setInterval 30)
+/settimezone - Set timezone to send reminders in correct time (setTimeZone Europe/Tallinn)
+/listtimezones - List available timezones (listTimeZones)
+/help - Get help (help)
 
 You can find the source code of this bot here: https://github.com/sergey-kruglov/dictionary-tgbot
 
